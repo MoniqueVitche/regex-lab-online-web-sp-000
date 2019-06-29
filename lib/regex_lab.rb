8e16a9e222ed4c3b8a7cbb2a000ca6b2f33abdb5
Monique Vitche <mvitche7@gmail.com>
2019-06-29 17:55:17 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,5 +1,8 @@
 def starts_with_a_vowel?(word)
-
+  if word.match(/^[aeiou]/i)
+    true
+  elsif word.match(^[b-d,f-h,j-n,p-t,v-z])
+      false
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
@@ -15,5 +18,6 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
+# has to have 10 digits, ignore spaces, hyphens and parentheses.
 
 end
