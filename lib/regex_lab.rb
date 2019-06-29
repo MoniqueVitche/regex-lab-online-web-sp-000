f24207717880b657e0d57606f7bb78c1b28349e6
Monique Vitche <mvitche7@gmail.com>
2019-06-29 17:58:25 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -6,18 +6,24 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  if text.scan(/\bun\w+ing\b/)
 end
 
 def words_five_letters_long(text)
-
+  text.scan(/\b[a-z]{5}\b/i)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  if text.match(/[A-Z].*(\.|\?|!)$/)
+    true
+  else
+    false
 end
 
 def valid_phone_number?(phone)
 # has to have 10 digits, ignore spaces, hyphens and parentheses.
-
+  if phone.match(/.*?\d{3}.*?\d{3}.*?\d{4}$/)
+    true
+  else
+    false
 end
